7155916a37f4ded7f367467d20f08994bbbb8c70
MVisentin1 <matteo.visentin@mail.mcgill.ca>
2026-03-16 23:32:36 +0000

    removed .Makefile.d from git tracking

--- .Makefile.d
+++
@@ -1,2 +0,0 @@
-src/LJF_Logic.vo src/LJF_Logic.glob src/LJF_Logic.v.beautified src/LJF_Logic.required_vo: src/LJF_Logic.v /home/cis5000/.opam/5.3.0/lib/rocq-runtime/rocqworker
-src/LJF_Logic.vos src/LJF_Logic.vok src/LJF_Logic.required_vos: src/LJF_Logic.v /home/cis5000/.opam/5.3.0/lib/rocq-runtime/rocqworker
