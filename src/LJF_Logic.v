1cdbd239f8d8e6eaa1ec8379db1a22dcac9b3a56
MVisentin1 <matteo.visentin@mail.mcgill.ca>
2026-03-30 20:08:54 +0000

    feat : completed refractoring of the previous type system, finished fibonnacci proofs

--- src/LJF_Logic.v
+++ src/LJF_Logic.v
@@ -2,6 +2,8 @@ From Stdlib Require Import List.
 From CARVe Require Import contexts.list algebras.dill.
 From VST.msl Require Import sepalg.
 
+Global Arguments upd_rel_ex  {R A} _ _ _ _.
+
 Inductive polarity : Type :=
 | Pos : polarity
 | Neg : polarity.
@@ -55,17 +57,14 @@ Inductive state : Type :=
 
 Definition ctx : Type := @lctx o mult.
 
-(* TODO use proper CARVe merge or update functions instead of cons *)
-
-                    (* bool : is rhs bracketed? *)
 Inductive ufc : ctx -> o -> state -> Prop :=
 | ufc_L_f :
-  forall {C: ctx} {N : o} {R : o},
+  forall {C: ctx} {N : o} {K : o},
     exh C ->
     has_entry C (N, omega) ->
     negative N ->
-    lfc C N R ->
-    ufc C R Bracketed
+    lfc C N K ->
+    ufc C K Bracketed
 | ufc_R_f :
   forall {C: ctx} {P: o},
     exh C ->
@@ -73,60 +72,56 @@ Inductive ufc : ctx -> o -> state -> Prop :=
     rfc C P ->
     ufc C P Bracketed
 | ufc_L_box :
-  forall {C Co C1: ctx} {A : o} {R : o} {s : state},
-    join ((A, omega) :: nil) C Co ->
-    join ((A, one) :: nil) C C1 ->
-    permeable A ->
-    ufc Co R s ->
-    ufc C1 R s
+  forall {C C1 : ctx} {B : o} {K : o} {s : state},
+    upd_rel_ex C (B, one) (B, omega) C1 ->
+    permeable B ->
+    ufc C1 K s ->
+    ufc C K s
 | ufc_R_box :
   forall {C: ctx} {D: o},
     bracketable D ->
     ufc C D Bracketed ->
     ufc C D Unbracketed
 | ufc_L_AndP :
-  forall {C CA CB C1: ctx} {A: o} {B: o} {R: o} {s : state},
-    join ((A, one) :: nil) C CA ->
-    join ((B, one) :: nil) CA CB ->
-    join ((AndP A B, one) :: nil) C C1 ->
-    ufc CB R s ->
-    ufc C1 R s
+  forall {C C1: ctx} {B1 B2 : o} {K: o} {s : state},
+    has_entry C ((AndP B1 B2), one) ->
+    upd_rel_ex C ((AndP B1 B2), one) ((AndP B1 B2), zero) C1 ->  
+    ufc ((B1, one) :: (B2, one) :: C1) K s ->
+    ufc C1 K s
 | ufc_R_AndN :
-  forall {C: ctx} {A: o} {B : o},
-    ufc C A Unbracketed ->
-    ufc C B Unbracketed->
-    ufc C (AndN A B) Unbracketed
+  forall {C: ctx} {B1 B2: o},
+    ufc C B1 Unbracketed ->
+    ufc C B2 Unbracketed->
+    ufc C (AndN B1 B2) Unbracketed
 | ufc_L_Or : 
-  forall {C CA CB C1: ctx} {A: o} {B: o} {R: o} {s: state},
-    join ((A, one) :: nil) C CA ->
-    join ((B, one) :: nil) C CB ->
-    join ((Or A B, one) :: nil) C C1 ->
-    ufc CA R s ->
-    ufc CB R s ->
-    ufc C1 R s
+  forall {C C1: ctx} {B1 B2 : o}  {K: o} {s: state},
+    has_entry C ((Or B1 B2), one) ->
+    upd_rel_ex C ((Or B1 B2), one) ((Or B1 B2), zero) C1 ->
+    ufc ((B1, one) :: C1) K s ->
+    ufc ((B2, one) :: C1) K s ->
+    ufc C K s
 | ufc_R_Impl :
-  forall {C C1: ctx} {A: o} {B: o},
-    join ((A, one) :: nil) C C1 ->
-    ufc C1 B Unbracketed ->
-    ufc C (Impl A B) Unbracketed
+  forall {C : ctx} {B: o} {E: o},
+    ufc ((B, one) :: C) E Unbracketed ->
+    ufc C (Impl B E) Unbracketed
 | ufc_L_True :
-  forall {C C1: ctx} {R: o} {s: state},
-    join ((True, one) :: nil) C C1 ->
-    ufc C R s ->
-    ufc C1 R s
+  forall {C C1: ctx}  {K: o} {s: state},
+    has_entry C (True, one) ->
+    upd_rel_ex C (True, one) (True, zero) C1 ->
+    ufc C1 K s ->
+    ufc C K s
 | ufc_L_False :
-  forall {C: ctx} {R: o} {s: state},
+  forall {C: ctx}  {K: o} {s: state},
     has_entry C (False, one) ->
-    ufc C R s
-(*First o for focus, second o for R*)
+    ufc C K s
+(*First o for focus, second o for K*)
 with lfc : ctx -> o -> o -> Prop :=
 | lfc_R_l :
-  forall {C C1: ctx} {P : o} {R : o},
-    join ((P, one) :: nil) C C1 ->
+  forall {C : ctx} {P : o}  {K : o},
     exh C ->
     positive P ->
-    ufc C1 R Bracketed ->
-    lfc C P R
+    ufc ((P, one) :: C) K Bracketed ->
+    lfc C P K
 | lfc_I_l :
   forall {C: ctx} {N : o},
     exh C ->
@@ -134,21 +129,21 @@ with lfc : ctx -> o -> o -> Prop :=
     atomic N ->
     lfc C N N
 | lfc_L_AndN_1 :
-  forall {C: ctx} {A1 A2 : o} {R : o},
+  forall {C: ctx} {B1 B2 : o}  {K : o},
     exh C -> 
-    lfc C A1 R ->
-    lfc C (AndN A1 A2) R
+    lfc C B1 K ->
+    lfc C (AndN B1 B2) K
 | lfc_L_AndN_2 :
-  forall {C: ctx} {A1 A2 : o} {R : o},
+  forall {C: ctx} {B1 B2 : o}  {K : o},
     exh C -> 
-    lfc C A2 R ->
-    lfc C (AndN A1 A2) R
+    lfc C B2 K ->
+    lfc C (AndN B1 B2) K
 | lfc_L_Impl : 
-  forall {C: ctx} {A B : o} {R : o}, 
+  forall {C: ctx} {B E : o}  {K : o}, 
     exh C ->
-    rfc C A ->
-    lfc C B R ->
-    lfc C (Impl A B) R
+    rfc C B ->
+    lfc C E K ->
+    lfc C (Impl B E) K
    
 with rfc : ctx -> o -> Prop :=
 | rfc_R_r :
@@ -164,21 +159,21 @@ with rfc : ctx -> o -> Prop :=
     atomic P ->
     rfc C P
 | rfc_R_AndP :
-  forall {C: ctx} {A: o} {B: o},
+  forall {C: ctx} {B1 B2: o},
     exh C ->
-    rfc C A ->
-    rfc C B ->
-    rfc C (AndP A B)
+    rfc C B1 ->
+    rfc C B2 ->
+    rfc C (AndP B1 B2)
 | rfc_R_Or_1 :
-  forall {C: ctx} {A1 A2: o},
+  forall {C: ctx} {B1 B2: o},
     exh C ->
-    rfc C A1 ->
-    rfc C (Or A1 A2)
+    rfc C B1 ->
+    rfc C (Or B1 B2)
 | rfc_R_Or_2 :
-  forall {C: ctx} {A1 A2: o},
+  forall {C: ctx} {B1 B2: o},
     exh C ->
-    rfc C A2 ->
-    rfc C (Or A1 A2)
+    rfc C B2 ->
+    rfc C (Or B1 B2)
 | rfc_R_True :
   forall {C: ctx},
     exh C ->
@@ -192,17 +187,21 @@ Lemma True_proveable: (rfc nil True).
 Qed.
 
 Ltac T_exh := 
-  simpl; 
-  repeat split; 
-  try (apply halo); 
-  try (apply halz); 
-  try (apply I)
+  match goal with
+    | [|- exh ?C ] => simpl; 
+      repeat split; 
+      try (apply halo); 
+      try (apply halz); 
+      try (apply I)
+    | _ => fail "Goal is not an exh predicate"
+  end
 .
 
+(*Use when we know what we are looking for, not for making decisions*)
 Ltac T_has_entry :=
   match goal with
-  | [|- has_entry ?G (?a, ?m)] => simpl; repeat (left; reflexivity || right)
-  | _ => fail
+  | [|- has_entry ?C (?a, ?m)] => simpl; repeat ((left; reflexivity) || right)
+  | _ => fail "Goal is not a entry lookup predicate"
   end
 .
 
@@ -210,17 +209,77 @@ Lemma Fibonnaci_forward_chaining : forall (x y z : nat),
   let a := Atom Pos x in
   let b := Atom Pos y in
   let c := Atom Neg z in
-  let G := (a, omega) :: (Impl a b, omega) :: (Impl b c, omega) :: nil in
-  lfc G (Impl a b) c.
+  let C := (a, omega) :: (Impl a b, omega) :: (Impl b c, omega) :: nil in
+  lfc C (Impl a b) c.
 Proof. 
-  intros x y z a b c G.
+  intros x y z a b c C.
   apply lfc_L_Impl.
     - T_exh.
-    - apply rfc_I_r.
+    - apply rfc_I_r. 
       + T_exh.
       + T_has_entry.
       + constructor.
+      + constructor. 
+    - apply lfc_R_l.
+      + T_exh.
+      + constructor. 
+      + eapply ufc_L_box.
+        -- eexists. constructor.
+        -- apply Permeable_pos_atom.
+          ++ constructor.
+          ++ constructor.
+        -- eapply ufc_L_f.
+          ++ T_exh.
+          ++ simpl. right. right. right. left. reflexivity.
+          ++ constructor.
+          ++ apply lfc_L_Impl.
+            --- T_exh.
+            --- apply rfc_I_r.
+              +++ T_exh.
+              +++ T_has_entry.
+              +++ constructor.
+              +++ constructor.
+            --- apply lfc_I_l.
+              +++ T_exh.
+              +++ constructor.
+              +++ constructor.
+Qed.
+
+            
+Lemma Fibonnaci_backward_chaining : forall (x y z : nat),
+  let a := Atom Pos x in
+  let b := Atom Neg y in
+  let c := Atom Neg z in
+  let C := (a, omega) :: (Impl a b, omega) :: (Impl b c, omega) :: nil in
+  lfc C (Impl b c) c.
+Proof. 
+  intros x y z a b c C.
+  apply lfc_L_Impl.
+    - T_exh.
+    - apply rfc_R_r.
+      + T_exh.
+      + apply ufc_R_box.
+        -- apply Bracketable_neg_atom.
+          ++ constructor.
+          ++ constructor.
+        -- eapply ufc_L_f.
+          ++ T_exh.
+          ++ simpl. right. left. reflexivity.
+          ++ constructor.
+          ++ apply lfc_L_Impl.
+            --- T_exh.
+            --- apply rfc_I_r.
+              +++ T_exh.
+              +++ T_has_entry.
+              +++ constructor.
+              +++ constructor.
+            --- apply lfc_I_l.
+              +++ T_exh.
+              +++ constructor.
+              +++ constructor.
+    - apply lfc_I_l.
+      + T_exh. 
       + constructor.
-    - eapply (@lfc_R_l G _ b c).
-      + simpl. constructor. 
+      + constructor.
+Qed.
 
