005cb0144753095fd0dc6ebd4b3fd9203895d9cc
MVisentin1 <matteo.visentin@mail.mcgill.ca>
2026-04-02 20:05:08 +0000

    feat : added T_rfc to right focus. Removed useless pattern match variables, corrected the rfc_R_r rule

--- src/LJF_Logic.v
+++ src/LJF_Logic.v
@@ -149,6 +149,7 @@ with rfc : ctx -> o -> Prop :=
 | rfc_R_r :
   forall {C: ctx} {N: o},
     exh C ->
+    negative N -> 
     ufc C N Unbracketed ->
     rfc C N
 | rfc_I_r :
@@ -162,7 +163,7 @@ with rfc : ctx -> o -> Prop :=
   forall {C: ctx} {B1 B2: o},
     exh C ->
     rfc C B1 ->
-    rfc C B2 ->
+    rfc C B2 ->   
     rfc C (AndP B1 B2)
 | rfc_R_Or_1 :
   forall {C: ctx} {B1 B2: o},
@@ -181,82 +182,88 @@ with rfc : ctx -> o -> Prop :=
 .
 
 
-Ltac T_exh := 
+Ltac T_exh := solve [
   match goal with
-    | [|- exh ?C ] => solve [simpl; 
-      repeat split; 
-      try (apply halo); 
-      try (apply halz); 
-      try (apply I)]
-    | _ => fail "Goal is not an exh predicate"
-  end
+    | [|- exh _ ] => simpl; repeat split; try (apply halo); try (apply halz); try (apply I)
+  end] || fail "Context isn't exhausted or Goal is not an exh lookup predicate"
 .
 
 (*Use when we know what we are looking  for, not for making decisions*)
-Ltac T_has_entry :=
+Ltac T_has_entry := solve [
   match goal with
-  | [|- has_entry ?C (?a, ?m)] => solve [simpl; repeat ((left; reflexivity) || right)]
-    || fail "Goal is not an entry lookup predicate"
+  | [|- has_entry _ _] => simpl; repeat ((left; reflexivity) || right) 
+  end] || fail "Goal is not an entry lookup predicate"
+.
+
+Ltac T_positive := solve 
+  [match goal with
+  | [|- positive (Atom Pos _)] => apply Pos_atom ; apply Is_atom
+  | [|- positive True] => apply Pos_true
+  | [|- positive False] => apply Pos_false
+  | [|- positive (AndP _ _)] => apply Pos_and
+  | [|- positive (Or _ _)] => apply Pos_or
+  end] || fail "Goal is not a positive predicate, or require prooving positivity of a negative formula"
+.
+
+Ltac T_negative := solve
+  [match goal with
+  | [|- negative (Atom Neg _)] => apply Neg_atom; apply Is_atom
+  | [|- negative (AndN _ _)] => apply Neg_and
+  | [|- negative (Impl _ _)] => apply Neg_imp
+  end] || fail "Goal is either not a negative predicate, or require prooving negativity of a positive formula"
+.
+
+Ltac T_permeable := solve 
+  [match goal with 
+  | [|- permeable (Atom Pos _)] => apply Permeable_pos_atom ; [> apply Is_atom | apply Pos_atom]
+  | [|- permeable _] => apply Permeable_neg ; T_negative
+  end] || fail "Goal is either not a permeable predicate, or require prooving permeability of a positive non-atom"
+.
+
+Ltac T_bracketable := solve
+  [match goal with
+  | [|- bracketable (Atom Neg _)] => apply Bracketable_neg_atom ; [> apply Is_atom | apply Neg_atom ]
+  | [|- bracketable _] => apply Bracketable_pos ; T_positive
+  end] || fail "Goal is either not a bracketable predicate, or require prooving bracketability of a positive non-atom"
+.
+
+(* Maybe consider converting this in Ltac2, seems like exceptions can be treaten more cleanly *)
+(*T_rfc goes through the Right focus phase. Leaves an ufc subgoal when the phase ends *)
+Ltac T_rfc := 
+  match goal with
+
+    (* Right focus on positive atom, must solve the branch*)
+    | [|- rfc _ (Atom Pos _)] => solve [apply rfc_I_r ; [> T_exh | T_has_entry | apply Pos_atom | apply Is_atom]]
+
+    (* Right focus on True, must solve the branch*)
+    | [|- rfc _ True] => solve [apply rfc_R_True ; T_exh]
+
+    (* Right focus on positive conjunction*)
+    | [|- rfc _ (AndP _ _)] => apply rfc_R_AndP ; [> T_exh | T_rfc | T_rfc]
+
+    (* Right focus on disjunction. Try proving B1, if it fails, try proving B2*)
+    | [|- rfc _ (Or _ _)] => (apply rfc_R_Or_1 ; [> T_exh | T_rfc]) || (apply rfc_R_Or_2 ; [> T_exh | T_rfc])
+
+    (* Right focus on negative formula, ends right-focus phase, leaves a ufc subgoal*)
+    | [|- rfc _ _] => apply rfc_R_r ; [> T_exh | T_negative | idtac]
   end
 .
 
-Ltac T_positive := match goal with
-  | [|- positive ?a ] => solve [
-      (apply Pos_atom ; apply Is_atom ) |
-      (apply Pos_true) |
-      (apply Pos_false) |
-      (apply Pos_and) |
-      (apply Pos_false)
-  ] || fail "Given predicate is not positive"
+(*T_lfc goes through the Left Focus phase. Leaves an ufc subgoal when the phase ends*)
+Ltac T_lfc := 
+  match goal with
+
+    (* Left focus on negative atom, must solve the branch*)
+    | [|- lfc _ (Atom Neg _) _] => solve [apply lfc_I_l ; [>  T_exh | apply Neg_atom ; apply Is_atom | apply Is_atom ]]
+
+    (* Left focus on negative conjunction. Try proving B1, if it fails, try proving B2*)
+    | [|- lfc _ (AndN _ _) _] => (apply lfc_L_AndN_1 ; [>  T_exh | T_lfc ]) || (apply lfc_L_AndN_2 ; [> T_exh | T_lfc ])
+
+    (* Left focus on implication*)
+    | [|- lfc _ (Impl _ _) _] => apply lfc_L_Impl ; [> T_exh | T_rfc | T_lfc]
+
+    (* Left focus on a positive formula, ends left-focus phase, leaves a ufc subgoal*)
+    | [|- lfc _ _ _] => apply lfc_R_l ; [> T_exh | T_positive | idtac ]
   end
 .
 
-Ltac T_negative := match goal with
-  | [|- negative ?a ] => solve [
-      (apply Neg_atom ; apply Is_atom ) |
-      (apply Neg_and) |
-      (apply Neg_imp)
-  ] || fail "Given predicate is not negative"
-  end
-.
-
-Ltac T_permeable := match goal with
-  | [|- permeable ?a ] => solve [
-        (apply Permeable_pos_atom ; [> apply Is_atom | apply Pos_atom ]) |
-        (apply Permeable_neg; constructor)
-    ] || fail "Given predicate is not permeable"
-  end
-.
-
-Ltac T_bracketable := match goal with
-  | [|- bracketable ?a ] => solve [
-        (apply Bracketable_neg_atom ; [> apply Is_atom | apply Neg_atom ]) |
-        (apply Bracketable_pos; constructor)
-    ] || fail "Given predicate is not bracketable"
-  end
-.
-
-
-(* TODO : Maybe consider converting this in Ltac2, seems like exceptions can be treaten more cleanly *)
-Ltac rec T_lfc_async := match goal with
-  | [|- lfc ?C (Atom Neg ?n) ?K] => 
-    solve [apply lfc_I_l ; 
-      [>  T_exh | apply Neg_atom ; apply Is_atom | apply Is_atom ]] 
-      || fail "Proof search failure : Cannot use lfc_I_l"
-  | [|- lfc ?C (AndN ?B1 ?B2) ?K] => 
-    solve [apply lfc_L_AndN_1 ;
-      [>  T_exh | T_lfc_async ]] 
-    || solve [apply lfc_L_AndN_2 ;
-      [> T_exh | T_lfc_async ]]
-  | [|- lfc ?C (Impl ?B1 ?B2) ?K] => 
-    solve [apply lfc_L_Impl ;
-      [> T_exh | T_rfc_async | T_lfc_async]]
-  | [|- lfc ?C ?P ?K] => 
-    solve [apply lfc_R_l ;
-      [> T_exh | T_positive | T_ufc]]
-  end
-with T_rfc_async := idtac
-with T_ufc := idtac.
-
-
-  
