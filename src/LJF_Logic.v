381dc9dffa02ffdb6bcaf926d81c3836efb6cad9
MVisentin1 <matteo.visentin@mail.mcgill.ca>
2026-04-03 07:09:53 +0000

    fix : added call by value evaluation to focusing tactics. Modified Fibonnaci proofs to use the focusing tactics

--- src/LJF_Logic.v
+++ src/LJF_Logic.v
@@ -199,7 +199,7 @@ Ltac T_positive := solve
   [match goal with
   | [|- positive ?a] => let a' := (eval cbv delta in a) in
     match a' with
-    | Atom Pos _ => apply Pos_atom ; apply Is_atom
+    | Atom Pos _ => apply Pos_atom
     | True => apply Pos_true
     | False => apply Pos_false
     | AndP _ _ => apply Pos_and
@@ -212,7 +212,7 @@ Ltac T_negative := solve
   [match goal with
   | [|- negative ?a] => let a' := (eval cbv delta in a) in
     match a' with
-    | Atom Neg _ => apply Neg_atom; apply Is_atom
+    | Atom Neg _ => apply Neg_atom
     | AndN _ _ => apply Neg_and
     | Impl _ _ => apply Neg_imp
     end
@@ -224,7 +224,7 @@ Ltac T_permeable := solve
   [match goal with 
   | [|- permeable ?a ] => let a' := (eval cbv delta in a) in
     match a' with
-    | Atom Pos _ => apply Permeable_pos_atom; [apply Is_atom | apply Pos_atom]
+    | Atom Pos _ => apply Permeable_pos_atom; [> apply Is_atom | apply Pos_atom]
     | _ => apply Permeable_neg; T_negative
     end
   end] || fail "Goal is either not a permeable predicate, or require prooving permeability of a positive non-atom".
@@ -243,39 +243,45 @@ Ltac T_bracketable := solve
 (*T_rfc goes through the Right focus phase. Leaves an ufc subgoal when the phase ends *)
 Ltac T_rfc := 
   match goal with
-
+  | [|- rfc ?c ?b] => let b' := (eval cbv delta in b) in
+    lazymatch b' with
     (* Right focus on positive atom, must solve the branch*)
-    | [|- rfc _ (Atom Pos _)] => solve [apply rfc_I_r ; [> T_exh | T_has_entry | apply Pos_atom | apply Is_atom]]
+    | Atom Pos _ => solve [apply rfc_I_r ; [> T_exh | T_has_entry | apply Pos_atom | apply Is_atom]]
 
     (* Right focus on True, must solve the branch*)
-    | [|- rfc _ True] => solve [apply rfc_R_True ; T_exh]
+    | True => solve [apply rfc_R_True ; T_exh]
 
     (* Right focus on positive conjunction*)
-    | [|- rfc _ (AndP _ _)] => apply rfc_R_AndP ; [> T_exh | T_rfc | T_rfc]
+    | AndP _ _ => apply rfc_R_AndP ; [> T_exh | T_rfc | T_rfc]
 
     (* Right focus on disjunction. Try proving B1, if it fails, try proving B2*)
-    | [|- rfc _ (Or _ _)] => (apply rfc_R_Or_1 ; [> T_exh | T_rfc]) || (apply rfc_R_Or_2 ; [> T_exh | T_rfc])
+    | Or _ _ => (apply rfc_R_Or_1 ; [> T_exh | T_rfc]) || (apply rfc_R_Or_2 ; [> T_exh | T_rfc])
 
     (* Right focus on negative formula, ends right-focus phase, leaves a ufc subgoal*)
-    | [|- rfc _ _] => apply rfc_R_r ; [> T_exh | T_negative | idtac]
+    | _ => apply rfc_R_r ; [> T_exh | T_negative | idtac]
   end
+end
 .
+
 
 (*T_lfc goes through the Left Focus phase. Leaves an ufc subgoal when the phase ends*)
-Ltac T_lfc := 
-  match goal with
-
+Ltac  T_lfc :=
+  match goal with 
+  | [|- lfc ?c ?b ?k] => let b' := (eval cbv delta in b) in
+    lazymatch b' with
     (* Left focus on negative atom, must solve the branch*)
-    | [|- lfc _ (Atom Neg _) _] => solve [apply lfc_I_l ; [>  T_exh | apply Neg_atom ; apply Is_atom | apply Is_atom ]]
+    | Atom Neg _ => solve [apply lfc_I_l ; [>  T_exh | apply Neg_atom | apply Is_atom ]]
 
     (* Left focus on negative conjunction. Try proving B1, if it fails, try proving B2*)
-    | [|- lfc _ (AndN _ _) _] => (apply lfc_L_AndN_1 ; [>  T_exh | T_lfc ]) || (apply lfc_L_AndN_2 ; [> T_exh | T_lfc ])
+    | AndN _ _ => (apply lfc_L_AndN_1 ; [>  T_exh | T_lfc ]) || (apply lfc_L_AndN_2 ; [> T_exh | T_lfc ])
 
     (* Left focus on implication*)
-    | [|- lfc _ (Impl _ _) _] => apply lfc_L_Impl ; [> T_exh | T_rfc | T_lfc]
+    | Impl _ _ => apply lfc_L_Impl ; [> T_exh | T_rfc | T_lfc]
 
     (* Left focus on a positive formula, ends left-focus phase, leaves a ufc subgoal*)
-    | [|- lfc _ _ _] => apply lfc_R_l ; [> T_exh | T_positive | idtac ]
+    | _ => apply lfc_R_l ; [> T_exh | T_positive | idtac ]
+    end
   end
 .
+
 
--- src/LJF_Test.v
+++ src/LJF_Test.v
@@ -19,73 +19,16 @@ Lemma Fibonnaci_forward_chaining : forall (x y z : nat),
   lfc C (Impl a b) c.
 Proof.
   intros.
-  apply lfc_L_Impl.
-    - T_exh.
-    - apply rfc_I_r. 
+  T_lfc.
+  eapply ufc_L_box.
+    - eexists. constructor.
+    - T_permeable.
+    - eapply ufc_L_f.
       + T_exh.
-      + T_has_entry.
-      + T_positive.
-      + constructor. 
-    - apply lfc_R_l.
-      + T_exh.
-      + T_positive.
-      + eapply ufc_L_box.
-        -- eexists. constructor.
-        -- T_permeable.
-        -- eapply ufc_L_f.
-          ++ T_exh.
-          ++ simpl. right. right. right. left. reflexivity.
-          ++ T_negative.
-          ++ apply lfc_L_Impl.
-            --- T_exh.
-            --- apply rfc_I_r.
-              +++ T_exh.
-              +++ T_has_entry.
-              +++ T_positive.
-              +++ constructor.
-            --- apply lfc_I_l.
-              +++ T_exh.
-              +++ T_negative.
-              +++ constructor.
-Qed. 
-  
-  
-
-(*
-Proof. 
-  intros x y z a b c C.
-  apply lfc_L_Impl.
-    - T_exh.
-    - apply rfc_I_r. 
-      + T_exh.
-      + T_has_entry.
-      + constructor.
-      + constructor. 
-    - apply lfc_R_l.
-      + T_exh.
-      + constructor. 
-      + eapply ufc_L_box.
-        -- eexists. constructor.
-        -- T_permeable.
-        -- eapply ufc_L_f.
-          ++ T_exh.
-          ++ simpl. right. right. right. left. reflexivity.
-          ++ constructor.
-          ++ apply lfc_L_Impl.
-            --- T_exh.
-            --- apply rfc_I_r.
-              +++ T_exh.
-              +++ T_has_entry.
-              +++ constructor.
-              +++ constructor.
-            --- apply lfc_I_l.
-              +++ T_exh.
-              +++ constructor.
-              +++ constructor.
-Qed. 
-*)
-
-
+      + simpl. right. right. right. left. reflexivity.
+      + T_negative.
+      + T_lfc.
+Qed.
 
 Lemma Fibonnaci_backward_chaining : forall (x y z : nat),
   let a := Atom Pos x in
@@ -93,33 +36,17 @@ Lemma Fibonnaci_backward_chaining : forall (x y z : nat),
   let c := Atom Neg z in
   let C := (a, omega) :: (Impl a b, omega) :: (Impl b c, omega) :: nil in
   lfc C (Impl b c) c.
-Proof. 
-  intros x y z a b c C.
-  apply lfc_L_Impl.
-    - T_exh.
-    - apply rfc_R_r.
+Proof.
+  intros.
+  T_lfc.
+  apply ufc_R_box.
+    - T_bracketable.
+    - eapply ufc_L_f.
       + T_exh.
-      + constructor.
-      + apply ufc_R_box.
-        -- T_bracketable.
-        -- eapply ufc_L_f.
-          ++ T_exh.
-          ++ simpl. right. left. reflexivity.
-          ++ constructor.
-          ++ apply lfc_L_Impl.
-            --- T_exh.
-            --- apply rfc_I_r.
-              +++ T_exh.
-              +++ T_has_entry.
-              +++ constructor.
-              +++ constructor.
-            --- apply lfc_I_l.
-              +++ T_exh.
-              +++ constructor.
-              +++ constructor.
-    - apply lfc_I_l.
-      + T_exh. 
-      + constructor.
-      + constructor.
-Qed.
+      + simpl. right. left. reflexivity.
+      + T_negative.
+      + T_lfc.
+Qed. 
+  
+
 
